eb46dbfbe01ecd55205b04882b9b66558c6af131
anderson.bellini <anderson.bellini@anac.gov.br>
2019-04-06 23:31:07 -0300

    update forms

--- index.js
+++ index.js
@@ -1,18 +1,95 @@
 const express = require("express");
-
 const app = express();
+const bodyParser = require("body-parser");
+
+const sqlite = require("sqlite");
+
+const dbConnection = sqlite.open("banco.sqlite", { Promise });
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (request, response) => {
-  response.render("home");
+app.get("/", async (request, response) => {
+  const db = await dbConnection;
+  const categoriasDb = await db.all("select * from categorias;");
+  const vagas = await db.all("select * from vagas;");
+  const categorias = categoriasDb.map(cat => {
+    return {
+      ...cat,
+      vagas: vagas.filter(vaga => vaga.categoria === cat.id)
+    };
+  });
+
+  //console.log(categorias);
+
+  response.render("home", {
+    categorias
+  });
 });
 
-app.get("/vaga", (request, response) => {
-  response.render("vaga");
+app.get("/vaga/:id", async (request, response) => {
+  // console.log(request.params.id);
+  const db = await dbConnection;
+  const vaga = await db.get(
+    `select * from vagas where id=${request.params.id}`
+  );
+  //console.log(vaga);
+
+  response.render("vaga", {
+    vaga
+  });
 });
+
+app.get("/admin", (request, response) => {
+  response.render("admin/home");
+});
+
+app.get("/admin/vagas", async (request, response) => {
+  const db = await dbConnection;
+  const vagas = await db.all("select * from vagas;");
+  response.render("admin/vagas", { vagas });
+});
+
+app.get("/admin/vagas/delete/:id", async (req, res) => {
+  const db = await dbConnection;
+  await db.run(`delete from vagas where id = ${req.params.id}`);
+  res.redirect("/admin/vagas");
+});
+
+app.get("/admin/vagas/nova", async (req, res) => {
+  res.render("admin/nova-vaga");
+});
+
+app.post("/admin/vagas/nova", async (req, res) => {
+  const { titulo, descricao, categoria } = req.body;
+  const db = await dbConnection;
+  await db.run(
+    `Insert into vagas(categoria, titulo, descricao) values (${categoria},'${titulo}', '${descricao}' ) `
+  );
+  res.redirect("/admin/vagas");
+});
+
+const init = async () => {
+  const db = await dbConnection;
+  await db.run(
+    "create table if not exists categorias ( id INTEGER PRIMARY KEY, categoria TEXT); "
+  );
+  //const categoria = "Marketing team";
+  //await db.run(`Insert into categorias(categoria) values ('${categoria}')`);
+
+  await db.run(
+    "create table if not exists vagas ( id INTEGER PRIMARY KEY, categoria INTEGER, titulo TEXT, descricao TEXT); "
+  );
+  // const vaga = "Social Media (San Marino)";
+  // const descricao = "Vaga de Social Media do fullstack Lab";
+  // await db.run(
+  //   `Insert into vagas(categoria, titulo, descricao) values (2,'${vaga}', '${descricao}' ) `
+  // );
+};
+
+init();
 
 app.listen(3000, err => {
   if (err) {
