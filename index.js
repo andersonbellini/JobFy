03705d10d0f0f9133139a73da8e8efa83e1b345f
anderson.bellini <anderson.bellini@anac.gov.br>
2019-04-08 12:47:12 -0300

    Fixing view path

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const dbConnection = sqlite.open(
 
 const port = process.env.PORT || 3000; //Config ZEIT
 
+app.set("views", path.join(__dirname, "views"));
+
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
